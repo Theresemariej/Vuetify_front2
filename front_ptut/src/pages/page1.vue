db842b91b1b8ea0c4b5a7c0233bb8a9f87e4591c
Theresemariej <123539601+Theresemariej@users.noreply.github.com>
2024-04-23 10:49:58 +0200

    Update page1.vue

--- front_ptut/src/pages/page1.vue
+++ front_ptut/src/pages/page1.vue
@@ -1,11 +1,52 @@
-<script setup>
+<template>
+  <h1>{{verti.libelle}}
+     {{verti.id_reponse1}}
+     {{verti.non}}</h1>
+  <v-btn @click="redirection">bouton</v-btn>
+</template>
+
+<script>
+
+  export default {
+  data() {
+
+    return {
+      verti: [
+        {
+          id: null,
+          libelle: null,
+          oui: null,
+          non: null,
+          id_reponse1: null,
+          id_reponse2: null
+        }
+      ]
+    };
+  },
+  mounted() {
+    this.fetchVerti();
+  },
+  methods: {
+    fetchVerti() {
+      fetch(`http://localhost:8989/verti/getLibelle/1`, {
+        method: 'GET',
+      })
+        .then(response => response.json())
+        .then(data => {
+          this.verti = data;
+        })
+        .catch(error => {
+          console.error('Error fetching verti:', error);
+        });
+    },
+    redirection() {
+      console.log("ça marche pas");
+    }
+  }
+  };
 
 </script>
 
-<template>
-  $END$
-</template>
-
-<style scoped>
-
+<style>
 </style>
+
